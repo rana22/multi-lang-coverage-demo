5b20eca5d1879c834a532685088366c57ddc91e9
rana22 <ambar.rana123@gmail.com>
2025-11-26 13:50:02 -0500

    set base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const raw = process.env.BASE_PATH ?? '';
+const base = raw.endsWith('/') ? raw : raw + '/';   // force trailing slash
 
 export default defineConfig({
   plugins: [react()],
+	base
   // if you deploy to GitHub Pages under a subpath, set base here:
   // base: '/your-repo-name/',
 });
