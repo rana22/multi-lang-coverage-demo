1fca1388b138d5a6f1c137c8600ccc39463ca649
rana22 <ambar.rana123@gmail.com>
2025-11-28 12:06:33 -0500

    add configuration

---
+++ src/config/projects.ts
@@ -0,0 +1,7 @@
+export const COVERAGE_SOURCES = [
+  {
+    id: 'react-app-1',
+    repo: 'react-app-1',
+    url: 'https://raw.githubusercontent.com/rana22/multi-lang-coverage-demo/refs/heads/main/react-app-1/coverage/coverage-summary.json',
+  }
+];
